cf82562def8d8901a58e4b131f10da7e1efb0297
Adeola Adebowale <104995062+derkaiserr@users.noreply.github.com>
2024-03-06 09:10:01 +0100

    update

--- src/header.jsx
+++ src/header.jsx
@@ -37,23 +37,23 @@ export default function Header ({navs}){
     return(
         <div>
 
-                <div onClick={toggleHams}  className="hams  md:hidden max-md:fixed right-0 bg-gray-100 w-[60%] items-end flex flex-col max-md:top-0 max-md:pt-6 max-md:pb-10 max-md:px-6 z-40  ">
-        <div className={isToggle ? 'ham-top ham-top1' :'ham-top'}></div>
-           <div className={isToggle ? 'ham-mid hidden' :'ham-mid'}></div>
-        <div className={isToggle ? 'ham-bottom ham-bottom1' :'ham-bottom'}></div>
-                </div>
-        <div className="header flex max-md:flex-col lg:items-center  font-bold justify-between">
-            <div className="max-md:fixed max-md:top-0 z-40 max-md:text-2xl text-xl poppins max-md:py-4 max-md:bg-gray-100 max-md:left-0 max-md:pl-4 max-md:w-[60%] ">Kaiser<span className="text-[#6c06f2]">Folio</span></div>
-
-            <div className={` nav max-md:duration-300 max-md:fixed max-md:transition-all max-md:pt-10  max-md:py-6 max-md:pb-24  max-md:bg-white z-30 max-md:w-[100%] ${isToggle ? "max-md:translate-y-[-6rem]" : "max-md:mt-0 max-md:translate-y-[-24rem]"} max-md:ease-in    max-md:flex-col flex gap-[2rem]`}>
-
-                {navs.map((nav, index)=> 
-                    <p className="hover:text-[#6c06f2] text-gray-700 cursor-pointer" onClick={nav.function} key={index}>{nav.name}</p>
-                    )}
-              
-            </div>
-            <a href="mailto:oluadebojnr@gmail.com" className={`max-md:fixed  ${isToggle ? "max-md:mt-[6.8rem]" : "max-md:mt-[-20rem]"} transition-all z-30 duration-300 ease-in  hoverImg  bg-gray-800 hoverIm p-2 rounded-full box-content w-fit`}>  <img src={chatIcon} alt="" /> </a>
-                    </div>
+        <div onClick={toggleHams}  className="hams  md:hidden max-md:fixed right-0 bg-gray-100 w-[60%] items-end flex flex-col max-md:top-0 max-md:pt-6 max-md:pb-10 max-md:px-6 z-40  ">
+<div className={isToggle ? 'ham-top ham-top1' :'ham-top'}></div>
+   <div className={isToggle ? 'ham-mid hidden' :'ham-mid'}></div>
+<div className={isToggle ? 'ham-bottom ham-bottom1' :'ham-bottom'}></div>
         </div>
+<div className="header flex max-md:flex-col lg:items-center max-md:justify-normal font-bold justify-between">
+    <div className="max-md:fixed max-md:top-0 z-40 max-md:text-2xl text-xl poppins max-md:py-4 max-md:bg-gray-100 max-md:left-0 max-md:pl-4 max-md:w-[60%] ">Kaiser<span className="text-[#6c06f2]">Folio</span></div>
+
+    <div className={` nav max-md:duration-300 max-md:fixed  max-md:pt-10  max-md:pb-24 max-md:bg-white z-30 max-md:w-[100%] ${isToggle ? "pullDown" : "pushUp"}     max-md:flex-col flex gap-[2rem]`}>
+
+        {navs.map((nav, index)=> 
+            <p className="hover:text-[#6c06f2] text-gray-700 cursor-pointer" onClick={nav.function} key={index}>{nav.name}</p>
+            )}
+      
+    </div>
+    <a href="mailto:oluadebojnr@gmail.com" className={`max-md:fixed  ${isToggle ? "chatDown" : "chatUp"} transition-all z-30 duration-300 ease-in  hover  bg-gray-800 p-2 rounded-full box-content w-fit`}>  <img src={chatIcon} alt="" /> </a>
+            </div>
+</div>
     )
 }
