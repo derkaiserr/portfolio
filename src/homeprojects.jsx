d4e28501c3e63809b930e9d457e804edbf11b2a6
derkaiserr <oluadebojnr@gmail.com>
2024-06-18 06:43:55 +0100

    added kaiserfinance to the portfolio

--- src/homeprojects.jsx
+++ src/homeprojects.jsx
@@ -14,9 +14,16 @@ const HomeProjects = React.forwardRef((props, ref) => {
       title: "Todo List(mobile)",
       desc: "A task management mobile-only web app which helps you keep track of your daily tasks.",
       link: "https://todoapp-derkaiser.netlify.app",
-      //   height: "18rem",
       width: "270rem",
-      github: 'https://github.com/derkaiserr/todoApp'
+      github: "https://github.com/derkaiserr/todoApp",
+    },
+    {
+      img: "../src/assets/kfin.png",
+      title: "KaiserFinance",
+      desc: "A finance management webapp designed to help users manage their income and expenses efficiently. ",
+      link: "https://kaiserfinance.netlify.app/",
+      width: "270rem",
+      github: "https://github.com/derkaiserr/kaiserfinance",
     },
     {
       img: Iptracker,
@@ -24,7 +31,7 @@ const HomeProjects = React.forwardRef((props, ref) => {
       desc: "An IP/Domain Tracker which provides information on IP Address, Server location, Timezone of Server, and Internet Service Provider of the User.",
       link: "https://iptracker-derkaiserr.netlify.app",
       width: "",
-      github: "https://github.com/derkaiserr/IPTracker"
+      github: "https://github.com/derkaiserr/IPTracker",
     },
     {
       width: "",
@@ -32,7 +39,7 @@ const HomeProjects = React.forwardRef((props, ref) => {
       title: "Atoms",
       desc: "An e-commerce landing page for footwear sales.",
       link: "https://atoms-derkaiserr.netlify.app",
-      github: "https://github.com/derkaiserr/atoms"
+      github: "https://github.com/derkaiserr/atoms",
     },
     {
       img: Payment,
@@ -40,16 +47,14 @@ const HomeProjects = React.forwardRef((props, ref) => {
       title: "Payment Platform",
       desc: "A beautifully designed payment platform for transactions after purchase.",
       link: "https://derkaiserr.github.io/frontEndMentorChallenge1",
-      github: "https://github.com/derkaiserr/frontEndMentorChallenge1"
+      github: "https://github.com/derkaiserr/frontEndMentorChallenge1",
     },
   ];
 
   return (
     <div ref={ref} className="pt-[4rem] md:pt-0 md:mt-4">
-
       <div className="text-6xl max-md:text-5xl max-md:mb-10 mb-16 tracking-tight font-bold">
         Projects<span className="text-[#6c06f2]">.</span>
-      
       </div>
 
       <div className="grid-cols-2 max-md:flex-col items-end max-md:flex grid max-md:gap-20 gap-12">
@@ -67,23 +72,27 @@ const HomeProjects = React.forwardRef((props, ref) => {
                 width={item.width}
                 alt=""
               />
-                <a className="float-right text-[#6c06f2] hover:scale-125 transition-all ease-in-out " target="_blank"  href={item.github}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="28"
-            height="28"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-github"
-          >
-            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-            <path d="M9 18c-4.51 2-5-2-7-2" />
-          </svg>
-        </a>
+              <a
+                className="float-right text-[#6c06f2] hover:scale-125 transition-all ease-in-out "
+                target="_blank"
+                href={item.github}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-github"
+                >
+                  <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                  <path d="M9 18c-4.51 2-5-2-7-2" />
+                </svg>
+              </a>
               <div>
                 <p className="font-bold text-2xl">
                   {item.title}{" "}
