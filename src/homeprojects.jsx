a6d6c8e77fd8346db0940a87d8d4c731aea48a5f
Adeola Adebowale <oluadebojnr@gmail.com>
2026-06-17 21:31:42 +0100

    update

--- src/homeprojects.jsx
+++ src/homeprojects.jsx
@@ -5,12 +5,21 @@ import Atoms from "./assets/atoms.png";
 import Payment from "./assets/payment.png";
 import kfin from "./assets/finance.png";
 import kanban from "./assets/Kanban.png";
+import engageX from "./assets/engagex.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const HomeProjects = React.forwardRef((props, ref) => {
   const data = [
+    {
+      img: engageX,
+      title: "Engage X",
+      desc: "AI-powered public speaking coach that provides practice sessions, personalized feedback, and performance insights to help users become more confident communicators.",
+      link: "https://engagexai.io/",
+
+
+    },
     {
       img: Todo,
       title: "Todo List(mobile)",
@@ -88,7 +97,7 @@ const HomeProjects = React.forwardRef((props, ref) => {
                  <p>{item.title}</p>{" "}
                   
                   <div className="ml-2 text-xl flex items-center  text-[#6c06f2]">
-                  <a
+              { item.github &&   <a
                 className=" text-[#6c06f2] hover:scale-125 transition-all ease-in-out "
                 target="_blank"
                 href={item.github}
@@ -108,7 +117,7 @@ const HomeProjects = React.forwardRef((props, ref) => {
                   <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
                   <path d="M9 18c-4.51 2-5-2-7-2" />
                 </svg>
-              </a>
+              </a>}
                     <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                   </div>
                 </div>
